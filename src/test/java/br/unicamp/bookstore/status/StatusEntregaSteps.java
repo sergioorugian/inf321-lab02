8aa7263ed18cc496f994a4d17939e291ce44720f
esoft28 - Hugo Giovanni Iuliano <hugo.iuliano@gmail.com>
2017-10-10 06:46:42 -0300

    push status

--- src/test/java/br/unicamp/bookstore/status/StatusEntregaSteps.java
+++ src/test/java/br/unicamp/bookstore/status/StatusEntregaSteps.java
@@ -1,14 +1,5 @@
 package br.unicamp.bookstore.status;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.ThrowableAssert.catchThrowable;
-
-import java.util.List;
-import java.util.Map;
-
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -18,18 +9,18 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 
 import br.unicamp.bookstore.Configuracao;
 import br.unicamp.bookstore.model.Endereco;
+import br.unicamp.bookstore.model.StatusEntrega;
 import br.unicamp.bookstore.service.BuscaEnderecoService;
 import cucumber.api.DataTable;
 import cucumber.api.PendingException;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.pt.Dado;
-import cucumber.api.java.pt.E;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
 
 public class StatusEntregaSteps {
-
+	
 	public WireMockServer wireMockServer;
 
 	@Mock
@@ -38,21 +29,20 @@ public class StatusEntregaSteps {
 	@InjectMocks
 	private BuscaEnderecoService buscaEnderecoService;
 
-	private Endereco endereco;
+	private StatusEntrega statusEntrega;
 
-	private String cep;
+	private String codigoRastreio;
 
 	private Throwable throwable;
-
-
+	
 	@Before
 	public void setUp() {
 		wireMockServer = new WireMockServer(9876);
 		wireMockServer.start();
 		MockitoAnnotations.initMocks(this);
 		Mockito.when(configuration.getBuscarEnderecoUrl()).thenReturn("http://localhost:9876/ws");
-		endereco = null;
-		cep = null;
+		statusEntrega = null;
+		codigoRastreio = null;
 		throwable = null;
 	}
 
@@ -60,11 +50,84 @@ public class StatusEntregaSteps {
 	public void teardown() {
 		wireMockServer.stop();
 	}
-
 	
-
 	@Dado("^um Código de rastreio válido:$")
-	public void umCódigoDeRastreioVálido(DataTable arg1) throws Throwable {
-		throw new PendingException();
+	public void um_Código_de_rastreio_válido(DataTable arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+	    throw new PendingException();
 	}
+
+	@Quando("^eu informo o codigo de rastreio na consulta de status$")
+	public void eu_informo_o_codigo_de_rastreio_na_consulta_de_status() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Então("^o resultado deve ser as informações do rastreio:$")
+	public void o_resultado_deve_ser_as_informações_do_rastreio(DataTable arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+	    throw new PendingException();
+	}
+
+	@Dado("^um Código de rastreio  não existente:$")
+	public void um_Código_de_rastreio_não_existente(DataTable arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+	    throw new PendingException();
+	}
+
+	@Quando("^eu informo o código de rastreio na consulta de status$")
+	public void eu_informo_o_código_de_rastreio_na_consulta_de_status() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Então("^o resultado deve ser o retorno da consulta:$")
+	public void o_resultado_deve_ser_o_retorno_da_consulta(DataTable arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+	    throw new PendingException();
+	}
+
+	@Dado("^um código de rastreio invalido inválido:$")
+	public void um_código_de_rastreio_invalido_inválido(DataTable arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+	    throw new PendingException();
+	}
+
+	@Quando("^eu informo o código de rastreio inválido:$")
+	public void eu_informo_o_código_de_rastreio_inválido() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Dado("^um código de rastreio válido:$")
+	public void um_código_de_rastreio_válido(DataTable arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+	    throw new PendingException();
+	}
+
+	@Dado("^o serviço sro não esta respondendo$")
+	public void o_serviço_sro_não_esta_respondendo() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+	
 }
